92d0ee224a5f58b55e6a88141c6b263d7f4511f1
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-05-14 00:23:53 +0600

    install neon and pg npm packages for connection with server

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,20 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import { Pool } from "pg";
+
 const app: Application = express();
 const port = 3000;
 
-//* Middleware */
+//? Middleware */
 app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true })); // for url
+
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_tTzU3ab9xBPG@ep-noisy-darkness-aq7t2d6v-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
@@ -15,9 +24,16 @@ app.get("/", (req: Request, res: Response) => {
     author: "Suzan Chandra",
   });
 });
-
+//? creating a post route */
 app.post("/", async (req: Request, res: Response) => {
-  console.log(req.body);
+  const { name, email, password } = req.body;
+  res.status(200).json({
+    message: "Data received successfully",
+    data: {
+      name,
+      email,
+    },
+  });
 });
 
 app.listen(port, () => {
