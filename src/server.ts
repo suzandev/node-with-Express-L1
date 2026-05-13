3b71f2e57f87de2cb654b3c071f8ab274f70524c
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-05-14 00:02:27 +0600

    refactor: update root route to return JSON response with author information

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,18 @@ import express, {
 const app: Application = express();
 const port = 3000;
 
+//* Middleware */
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.status(200).json({
+    message: "Hello World!",
+    author: "Suzan Chandra",
+  });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
