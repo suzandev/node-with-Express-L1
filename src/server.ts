814fd9c9d1d9e3911b8219fc292a3b00e58b4c47
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-05-13 23:41:27 +0600

    node express server create with npm packages

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
