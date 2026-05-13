8cf007d78f30c06fad6b836dabe6f23923190455
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-05-14 00:48:37 +0600

    initializing the database and creating the users table in Neon

--- src/server.ts
+++ src/server.ts
@@ -13,10 +13,35 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true })); // for url
 
+//? creating a pool for postgresql connection */
 const pool = new Pool({
   connectionString:
     "postgresql://neondb_owner:npg_tTzU3ab9xBPG@ep-noisy-darkness-aq7t2d6v-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
+
+//? initializing the database and creating the users table */
+const initDB = async () => {
+  try {
+    await pool.query(
+      `CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        email VARCHAR(20) UNIQUE,
+        password VARCHAR(20) NOT NULL,
+        is_active BOOLEAN DEFAULT TRUE,
+        age INT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+      `,
+    );
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+  }
+};
+
+initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
